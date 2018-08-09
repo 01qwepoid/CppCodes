e1afe14e924430255e9b0d63299aa0ee05138a6e
01qwepoid <khanna.sk29@gmail.com>
2018-08-09 21:16:22 +0530

    new code added

---
+++ GreatestCommonDivisor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    if (b != 0)
+       return gcd(b, a % b);
+    return a;
+}
+
+int main(){
+   int a, b;
+   cout << "Enter two positive integers: ";
+   cin >> a >> b;
+   cout << "G.C.D of " << a << " & " <<  b << " is: " << gcd(a,b);
+
+   return 0;
+}
+
